7748726c44b78653104c596a18e7ed549b089b55
Max Shron <max.shron@gmail.com>
2011-11-05 16:50:07 -0400

    Currently working!

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -17,24 +17,38 @@ var DocumentCollection = Backbone.Collection.extend({
 var Thread = Backbone.Model.extend({
     initialize: function () {
         this.revisions = [];
+        this.bind('all', this.orderRevs, this);
     },
-    addRevision: function (rev) {
+    addRevisionision: function (rev) {
         this.revisions.push(rev);
+    },
+    orderRevs: function () {
+        console.log('foo');
+        this.revisions = _(this.revisions)
+                          .sortBy(function (rev) { return rev.get('score') })
     }
 });
 
 var ThreadCollection = Backbone.Collection.extend({
     model: Thread,
-    url: function () {
-        return _.sprintf('/api/documents/%{docid}/threads', this.toJSON());
-    },
     comparator: function (th) {
+        var up = _(th.revisions)
+                  .chain()
+                  .map(function (rev) { return rev.get('upvotes') })
+                  .reduce(function (memo,num) { return memo+num }, 0)
+                  .value();
+        var down = _(th.revisions)
+                  .chain()
+                  .map(function (rev) { return rev.get('downvotes') })
+                  .reduce(function (memo,num) { return memo+num }, 0)
+                  .value();
+        return down-up;
     },
     initialize: function () {
         var that = this;
         window.revisions.each(function (x) {
             var th = that.get(x.get('root'));
-            th.addRevision(x);
+            th.addRevisionision(x);
           });
     }
 });
@@ -43,11 +57,11 @@ var ThreadCollection = Backbone.Collection.extend({
 
 var Revision = Backbone.Model.extend({
     upVote: function() {
-        this.set({upVotes: this.get('upVotes') + 1});
+        this.set({upVotes: this.get('upvotes') + 1});
         return this.upVotes
     },
     downVote: function() {
-        this.set({downVotes: this.get('downVotes') + 1});
+        this.set({downVotes: this.get('downvotes') + 1});
         return this.downVotes
     }
 });
@@ -58,7 +72,6 @@ var RevisionCollection = Backbone.Collection.extend({
         return '/api/documents/0/revisions'
     },
     initialize: function () {
-        this.bind('add', function(x) {console.log(x)});
     },
     genThreads: function () {
         window.threads = new ThreadCollection(
@@ -67,17 +80,14 @@ var RevisionCollection = Backbone.Collection.extend({
                 .map(function (x) { return {id: x.id} } )
                 .value()
           );
+        window.threads.sort();
     },
-    comparator: function (rev) {
-        return rev.get('score');
-    }
 });
 
 window.revisions = new RevisionCollection([{"parent": 0, "text": "hello world", "downvotes": 0, "score": 0.6, "upvotes": 1, "root": 0, "id": 0}, {"parent": 0, "text": "hello cruel world", "downvotes": 1, "score": 0.4, "upvotes": 1, "root": 0, "id": 1}, {"parent": 1, "text": "hello cruel cruel world", "downvotes": 0, "score": 0.8, "upvotes": 2, "root": 0, "id": 2}, {"parent": 3, "text": "I like ponies", "downvotes": 0, "score": 0.6, "upvotes": 1, "root": 3, "id": 3}]);
 
 
 window.revisions.genThreads();
-
 
 //------------------------------------------------------------------------
 
@@ -120,17 +130,45 @@ var User = Backbone.Model.extend({
 
 var ThreadView = Backbone.View.extend({
     tagName: "li",
+    template: _.template($('#thread-template').html()),
+    addRevision: function (rev) {
+       var rv = new RevisionView({model: rev});
+       this.rvs.push(rv);
+    },
     render: function () {
-        $(this.el).html(_.template("<%= topics %>", this.model.toJSON()));
-        _(this.revisions)
-            .each(function (rev) { rev.render(); });
-        return this
+        $(this.el).html(this.template(this.model.toJSON()));
+        var that = this;
+        _(this.rvs).each(function (rv) {
+            that.$('.revisions').append(rv.el)
+        });
+    },
+    initialize: function () {
+        this.rvs = [];
+        var that = this;
+        $('#threads').append(this.el);
+        _(this.model.revisions)
+         .each(function (rev) { that.addRevision(rev) });
+        this.model.bind('all', this.render, this);
     }
 });
 
 var RevisionView = Backbone.View.extend({
     tagName: "div",
+    template: _.template($('#revision-template').html()),
     render: function() {
-        $(this.el).html(_.template("<% text %>", this.model.toJSON()));
+        $(this.el).html(this.template(this.model.toJSON()));
+    },
+    initialize: function () {
+        this.model.bind('all', this.render, this);
     }
 });
+
+
+//------------------------------------------------------------------------
+
+window.threadViews = window.threads
+                        .map(function (th) {return new ThreadView({model: th})});
+
+
+window.threads.map(function (th) {th.trigger('go')});
+window.revisions.map(function (rev) {rev.trigger('go')});
