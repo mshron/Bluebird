efabfc3992036123288893c5b640b4de25b4df19
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-11-17 16:43:14 -0500

    api.py: Twitter oauth now works (sorta)
    Redirects to a login screen on API calls seem not to work, so
    for now we enforce Twitter login on access of /documents/doc_id

--- api.py
+++ api.py
@@ -31,15 +31,16 @@ twitter = oauth.remote_app('twitter',
     access_token_url='http://api.twitter.com/oauth/access_token',
     authorize_url='http://api.twitter.com/oauth/authenticate',
     consumer_key='QzW7VFbwH2uhUrVqdtwrA',
-    consumer_secret='Wxij3J8N0jkcDoCBVRPxPGEsm1JXw4JUY27atr4ZjE'
-)
+    consumer_secret='Wxij3J8N0jkcDoCBVRPxPGEsm1JXw4JUY27atr4ZjE')
 
 def put(attrs, obj_type=None):
     '''write an object in dictionary representation to datastore'''
     if obj_type:
         attrs['type'] = obj_type
     obj = data.parse(attrs)
-    print obj
+    # ensure current user is author of new revision
+    if hasattr(obj, 'author'):
+        obj.author = fl.g.user.key
     key = ds.put(obj)
     return key.id() if key else ''
 
@@ -52,17 +53,20 @@ def collection_handler(request, col_key, obj_type=None):
     '''Default handling for fl.requests on object urls:
     * Write object on post,
     * Retrieve list objects in collection on GET'''
-    if request.method in ['POST','PUT']:
+    app.logger.debug('[collection_handler] collection: %s, method: %s user: %s' % (col_key, fl.request.method, fl.g.user))
+    if fl.request.method in ['POST','PUT']:
+        # if fl.g.user is None:
+        #     app.logger.debug('[collection_handler] redirect')
+        #     return fl.redirect(fl.url_for('login', next=fl.request.url))
         return put(fl.request.json, obj_type=obj_type); 
     else:
         return fl.Response(js.dumps(get_all(col_key), cls=data.DataEncoder), mimetype='application/json')
-
 
 @app.before_request
 def before_request():
     fl.g.user = None
     if 'user_id' in fl.session:
-        user_key = '%s:%s' ('User', fl.session['user_id'])
+        user_key = '%s:%s' % ('User', fl.session['user_id'])
         fl.g.user = ds.get(user_key)
 
 @twitter.tokengetter
@@ -73,10 +77,6 @@ def get_twitter_token():
 
 @app.route('/login')
 def login():
-    """Calling into authorize will cause the OpenID auth machinery to kick
-in. When all worked out as expected, the remote application will
-redirect back to the callback URL provided.
-"""
     return twitter.authorize(callback=fl.url_for('oauth_authorized',
         next=fl.request.args.get('next') or fl.request.referrer or None))
 
@@ -99,7 +99,7 @@ def oauth_authorized(resp):
 
     # user never signed on
     if user is None:
-        user = data.User(key=user_key)
+        user = data.User(key=user_key, screen_name=resp['screen_name'])
 
     # in any case we update the authenciation token in the db
     # In case the user temporarily revoked access we will have
@@ -110,7 +110,7 @@ def oauth_authorized(resp):
 
     fl.session['user_id'] = user.key.id()
     fl.flash('You were signed in')
-    return redirect(next_url)
+    return fl.redirect(next_url)
 
 @app.route('/api/users',
             methods=['GET','POST'])
@@ -136,42 +136,51 @@ def revision(doc_id, rev_id):
     key = 'Document:%s:revisions' % doc_id
     return collection_handler(fl.request, key, obj_type='Revision')
 
-@app.route('/api/documents/<doc_id>/revisions/<rev_id>/vote', 
+@app.route('/api/documents/<doc_id>/revisions/<rev_id>', 
             methods=['PUT'])
 def vote(doc_id, rev_id):
     if fl.request.method == 'PUT':
-        # use default user for now
-        vote = fl.request.json['vote']
-        ukey = fl.request.json['user']
-        rkey = 'Revision:%s' % (rev_id)
-        dkey = 'Document:%s' % doc_id
-        up_voted = ds.inter(['%s:revisions' % dkey, '%s:up_voted' % ukey])
-        down_voted = ds.inter(['%s:revisions' % dkey, '%s:down_voted' % ukey])
-        # vote blanking is always ok
-        if vote==0 or (vote==-1 and (rkey in up_voted)) \
-           or (vote==1 and (rkey in down_voted)): 
-            ds.vote(ukey, rkey, vote)
-        else:
-            # allow vote if user has votes left, and has not voted in thread
-            rev = ds.get(rkey)
-            if (vote==1) and (len(up_voted) < MAX_UP):
-                if not ds.inter(['%s:revisions' % rev.root, '%s:up_voted' % ukey]):
-                    ds.vote(ukey, rkey, vote)
-                else:
-                    fl.abort(403)
-            elif (vote==-1) and (len(down_voted) < MAX_DOWN):   
-                if not ds.inter(['%s:revisions' % rev.root, '%s:down_voted' % ukey]):
-                    ds.vote(ukey, rkey, vote)
-                else:
-                    fl.abort(403)
+        vote = fl.request.args('vote', None)
+        if not vote is None:
+            user = fl.g.user
+            rkey = 'Revision:%s' % (rev_id)
+            dkey = 'Document:%s' % doc_id
+            up_voted = ds.inter(['%s:revisions' % dkey, '%s:up_voted' % user])
+            down_voted = ds.inter(['%s:revisions' % dkey, '%s:down_voted' % user])
+            # vote blanking is always ok
+            if vote==0 or (vote==-1 and (rkey in up_voted)) \
+               or (vote==1 and (rkey in down_voted)): 
+                ds.vote(user, rkey, vote)
             else:
-                fl.abort(403)
-        return fl.Response('Succes')
+                # allow vote if user has votes left, and has not voted in thread
+                rev = ds.get(rkey)
+                if (vote==1) and (len(up_voted) < MAX_UP):
+                    if not ds.inter(['%s:revisions' % rev.root, '%s:up_voted' % user]):
+                        ds.vote(user, rkey, vote)
+                    else:
+                        fl.abort(403)
+                elif (vote==-1) and (len(down_voted) < MAX_DOWN):   
+                    if not ds.inter(['%s:revisions' % rev.root, '%s:down_voted' % user]):
+                        ds.vote(user, rkey, vote)
+                    else:
+                        fl.abort(403)
+                else:
+                    fl.abort(403)
+            return fl.Response('Succes')
+        else:
+            fl.abort(403)
 
 @app.route('/documents/<doc_id>')
 def dochtml(doc_id):
+    if fl.g.user is None:
+        app.logger.debug('[collection_handler] redirect')
+        return fl.redirect(fl.url_for('login', next=fl.request.url))
     ddata = [obj.__dict__ for obj in get_all('Document:%s:revisions'%doc_id)]
-        return fl.render_template('test.html', data=ddata) #FIXME
+    return fl.render_template('test.html', data=ddata) #FIXME
 
 if __name__ == "__main__":
+    import logging
+    file_handler = logging.FileHandler('api.log')
+    file_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(file_handler)
     app.run()
